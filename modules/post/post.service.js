b27cb6fbc28e87a07af090c025f81b7fb5f66031
HenriqueRiciole <hriciole@gmail.com>
2023-07-16 11:35:54 -0300

    criado service e rotas de post

---
+++ modules/post/post.service.js
@@ -0,0 +1,16 @@
+import Post from './post.model'
+
+export const createPost= async(body, user)=>{
+    return await Post.create({
+        text:body.text,
+        createDate: new Date(),
+        createdBy: user.id 
+    })
+}
+
+export const getPosts= async( Limit= 10)=>{
+    return await Post.find()
+    .populate('createdBy','user' )
+    .sort({createdDate: -1})
+    .limit(Limit)
+}
