02938c4325b1205eb9e6fc821d42b85f60b6acee
HenriqueRiciole <hriciole@gmail.com>
2023-10-25 23:30:22 -0300

    criado rotas e service de post

---
+++ modules/post/post.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+import User from "../user/user.model"
+
+const PostSchema= new mongoose.Schema({
+    text:{ type: String, required: true, maxlength: 256},
+    createDate:{ type: Date, required: true},
+    createBy:{type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'}
+})
+
+export default mongoose.model.Post || mongoose.model( 'Post', PostSchema)
---
+++ modules/post/post.service.js
@@ -0,0 +1,14 @@
+import Post from './post.model'
+
+
+export const createPost= async (body, user) => {
+    return await Post.create({
+        text: body.text,
+        createDate: new Date(),
+        createBy: user.id
+    })
+}
+
+export const getPosts= async (limit= 10)=> {
+    return await Post.find().populate('createBy', 'user').sort({createdDate: -1}).limit(limit)
+}
