3ee96f1fd011db73e4b63432ae7ce05b9e41fa2b
HenriqueRiciole <hriciole@gmail.com>
2023-10-29 22:17:53 -0300

    criado menu para posts

--- src/components/cards/Post.jsx
+++ src/components/cards/Post.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import moment from "moment/moment";
 
+import Menu from "../navigation/menu";
+
 const PostContainer= styled.div`
   background-color: ${props=> props.theme.white};
   padding: 20px;
@@ -18,10 +20,36 @@ const StyledDate= styled.p`
 const ContainerText= styled.div`
   margin-top: 10px;
 `
+const ContainerMenu= styled.div`
+  float: right;
+`
 
 function Post({text, user, date}){
+  const handleEdit= ()=> {
+    console.log("EDITAR PUBLICAÇÃO")
+  }
+
+  const handleDelete= ()=> {
+    console.log("DELETAR PUBLICACAO")
+  }
+
+
   return(
     <PostContainer>
+        <ContainerMenu>
+          <Menu
+            options={[
+              {
+                text: 'Editar Publicação',
+                onClick: handleEdit
+              },
+              {
+                text: 'Deletar Publicação',
+                onClick: handleDelete
+              }
+            ]}
+          />
+        </ContainerMenu>       
         <StyledUsername>@{user}</StyledUsername>
         <StyledDate>{moment(date).format('LLL')}</StyledDate>
         <ContainerText>
